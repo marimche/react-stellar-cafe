cb823d2db245534adfa3fe8da89390cf0500b466
marimche <test@gmail.com>
2025-09-12 21:54:21 +0300

    update ingredients menu

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,10 +1,37 @@
-import { IngredientDetails } from '@/components/ingredient-detales/ingredient-detales';
+import { IngredientCard } from '@/components/ingredient-card/ingredient-card';
 import { Tab } from '@krgaa/react-developer-burger-ui-components';
+import { useEffect, useState } from 'react';
 
 import styles from './burger-ingredients.module.css';
 
-export const BurgerIngredients = ({ ingredients }) => {
-  // console.log(ingredients);
+export const BurgerIngredients = () => {
+  const url = 'https://norma.nomoreparties.space/api/ingredients ';
+
+  const [ingredients, setIngredients] = useState([]);
+  const [ingredientsByTypes, setIngredientsByTypes] = useState({});
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((ingredients) => {
+        setIngredients(ingredients.data);
+        setIngredientsByTypes(filterIngredientsByTypes(ingredients.data));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  }, []);
+
+  const filterIngredientsByTypes = (ingredients) => {
+    const ingredientsByTypes = {};
+    ingredients.forEach((ingredient) => {
+      if (!(ingredient.type in ingredientsByTypes)) {
+        ingredientsByTypes[ingredient.type] = [];
+      }
+      ingredientsByTypes[ingredient.type].push(ingredient);
+    });
+    return ingredientsByTypes;
+  };
 
   return (
     <section className={styles.burger_ingredients}>
@@ -16,6 +43,7 @@ export const BurgerIngredients = ({ ingredients }) => {
             onClick={() => {
               document.getElementById('bun').scrollIntoView({
                 behavior: 'smooth',
+                inline: 'nearest',
               });
             }}
           >
@@ -27,6 +55,7 @@ export const BurgerIngredients = ({ ingredients }) => {
             onClick={() => {
               document.getElementById('main').scrollIntoView({
                 behavior: 'smooth',
+                inline: 'nearest',
               });
             }}
           >
@@ -38,6 +67,7 @@ export const BurgerIngredients = ({ ingredients }) => {
             onClick={() => {
               document.getElementById('sauce').scrollIntoView({
                 behavior: 'smooth',
+                inline: 'nearest',
               });
             }}
           >
@@ -45,28 +75,40 @@ export const BurgerIngredients = ({ ingredients }) => {
           </Tab>
         </ul>
       </nav>
-      <div className={styles.items}>
-        <ul className={styles.list}>
-          <li id="bun">
-            Булки
-            <IngredientDetails ingredient={ingredients[0]} />
-          </li>
-          <li id="main">
-            Начинки
-            <div className={styles.items}>
-              <IngredientDetails ingredient={ingredients[3]} />
-            </div>
-          </li>
-          <li id="sauce">
-            Соусы
-            <div className={styles.items}>
-              {ingredients.map((ingredient) => (
-                <IngredientDetails key={ingredient._id} ingredient={ingredient} />
-              ))}
-            </div>
-          </li>
-        </ul>
-      </div>
+      {ingredients?.length > 0 && (
+        <div className={`${styles.items} mt-10`}>
+          <ul className={styles.list}>
+            <li id="bun">
+              <p className="text text_type_main-medium">Булки</p>
+              <div className={styles.items}>
+                {ingredientsByTypes['bun'].map((ingredient) => (
+                  <IngredientCard key={ingredient._id} ingredient={ingredient} />
+                ))}
+              </div>
+            </li>
+            <li id="main">
+              <p className="text text_type_main-medium">Начинки</p>
+              <div className={styles.items}>
+                {ingredientsByTypes['main'].map((ingredient) => (
+                  <IngredientCard
+                    key={ingredient._id}
+                    ingredient={ingredient}
+                    count={1}
+                  />
+                ))}
+              </div>
+            </li>
+            <li id="sauce">
+              <p className="text text_type_main-medium">Соусы</p>
+              <div className={styles.items}>
+                {ingredientsByTypes['sauce'].map((ingredient) => (
+                  <IngredientCard key={ingredient._id} ingredient={ingredient} />
+                ))}
+              </div>
+            </li>
+          </ul>
+        </div>
+      )}
     </section>
   );
 };
