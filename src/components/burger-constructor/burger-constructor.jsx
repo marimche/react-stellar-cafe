a7bb65ff28e82c56e19756dde5703f734e4404fa
marimche <test@gmail.com>
2025-09-19 22:36:38 +0300

    use redux for burger-constructor

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -6,13 +6,24 @@ import {
 } from '@krgaa/react-developer-burger-ui-components';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
+import {
+  getSelectedIngredients,
+  getSelectedBun,
+} from '../../services/burger-constructor/reducer';
 import { Modal } from '../modals/modal';
 import { OrderDetails } from '../order-details/order-details';
 
 import styles from './burger-constructor.module.css';
 
 export const BurgerConstructor = ({ ingredients }) => {
+  //список выбранных ингредиентов
+  const selectedIngredients = useSelector(getSelectedIngredients);
+  const bun = useSelector(getSelectedBun);
+
+  // const dispatch = useDispatch();
+
   const [visible, setVisible] = useState(false);
 
   const handleOpenOrderDetails = () => {
@@ -23,52 +34,68 @@ export const BurgerConstructor = ({ ingredients }) => {
     setVisible(false);
   };
 
-  const getBun = (ingredients) => {
-    const buns = ingredients.filter((ingredient) => ingredient.type === 'bun');
-    return buns[0];
-  };
-
-  const bun = getBun(ingredients);
-  const selectedIngredients = ingredients.filter(
-    (ingredient) => ingredient.type !== 'bun'
-  );
-
   return (
     <section className={styles.burger_constructor} id="react-modals">
       {ingredients?.length > 0 && (
         <div>
           <div className={styles.burger_ingredients}>
             <div className="ml-6">
-              <ConstructorElement
-                type="top"
-                isLocked={true}
-                text={bun.name}
-                price={bun.price}
-                thumbnail={bun.image}
-              />
+              {bun === null ? (
+                <ConstructorElement
+                  type="top"
+                  text="Выберите булки"
+                  isLocked={true}
+                  thumbnail={' '}
+                />
+              ) : (
+                <ConstructorElement
+                  type="top"
+                  isLocked={true}
+                  text={bun.name}
+                  price={bun.price}
+                  thumbnail={bun.image}
+                />
+              )}
             </div>
             <div className={styles.internal_ingredients}>
-              {selectedIngredients.map((ingredient) => {
-                return (
-                  <div key={ingredient._id}>
-                    <DragIcon type="primary" />
-                    <ConstructorElement
-                      text={ingredient.name}
-                      price={ingredient.price}
-                      thumbnail={ingredient.image}
-                    />
-                  </div>
-                );
-              })}
+              {selectedIngredients.length > 0 ? (
+                selectedIngredients.map((ingredient) => {
+                  return (
+                    <div key={ingredient._id + Math.random()}>
+                      <DragIcon type="primary" />
+                      <ConstructorElement
+                        text={ingredient.name}
+                        price={ingredient.price}
+                        thumbnail={ingredient.image}
+                      />
+                    </div>
+                  );
+                })
+              ) : (
+                <ConstructorElement
+                  text="Выберите начинку"
+                  isLocked={undefined}
+                  thumbnail={' '}
+                />
+              )}
             </div>
             <div className="ml-6">
-              <ConstructorElement
-                type="bottom"
-                isLocked={true}
-                text={bun.name}
-                price={bun.price}
-                thumbnail={bun.image}
-              />
+              {bun === null ? (
+                <ConstructorElement
+                  type="top"
+                  text="Выберите булки"
+                  isLocked={true}
+                  thumbnail={' '}
+                />
+              ) : (
+                <ConstructorElement
+                  type="bottom"
+                  isLocked={true}
+                  text={bun.name}
+                  price={bun.price}
+                  thumbnail={bun.image}
+                />
+              )}
             </div>
           </div>
           <div className={`${styles.total_price} mt-10`}>
